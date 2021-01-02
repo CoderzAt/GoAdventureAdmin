f7ac6627bf4e5987436470dc3bdccae267398f3c
Rajendar-Prathapagiri <67008808+Rajendar-Prathapagiri@users.noreply.github.com>
2021-01-02 15:32:53 +0530

    Create DisplayErrorMsg.js

---
+++ src/Shared/DisplayErrorMsg.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+class Displayerrormsg extends Component {
+    constructor(props) {
+        super(props);
+       this.state = {
+
+       }
+    }
+ render() {
+	    return (
+            <div>
+            {this.props.message ?
+            <div className={`message-wrapper ${this.props.messageData.isSuccess ? "success" : "error"}`}>{this.props.messageData.message.map(obj => (<li>{obj.message}</li>))}</div> :
+            null}
+            </div>
+        )
+        }
+    }
+export default Displayerrormsg
